0fd09c87b5980ab22b914ef47b94eb4849212386
chentiangang <972581034@qq.com>
2024-11-22 11:20:57 +0800

    update

--- test/test.go
+++ test/test.go
@@ -3,19 +3,17 @@ package main
 import (
 	"fmt"
 	"marketdata"
+	"time"
 )
 
 func main() {
 	client := marketdata.NewDefaultClient()
 
 	client.RealTimeQuotes.SetSymbols([]string{"0.002957"})
-	ch := client.RealTimeQuotes.Fetch()
 	defer client.RealTimeQuotes.Close()
 
-	for {
-		select {
-		case quote := <-ch:
-			fmt.Println(quote)
-		}
+	for data := range client.RealTimeQuotes.Fetch() {
+		fmt.Println(time.Now().Format("2006-01-02 15:04:05"), data)
 	}
+
 }
