173fd1ff752d9ebbc7db6fa6dfe5261b3213d520
chentiangang <972581034@qq.com>
2024-11-23 15:42:42 +0800

    update

--- test/test.go
+++ test/test.go
@@ -3,26 +3,35 @@ package main
 import (
 	"fmt"
 	"marketdata"
-	"time"
 )
 
 func main() {
 	mkcli := marketdata.NewDefaultClient()
 
-	mkcli.RealTimeQuotes.Set([]string{"0.002957", "1.600360"})
-	defer mkcli.RealTimeQuotes.Close()
-
-	line, err := mkcli.Kline.Fetch("0.002957", "15", "120")
+	markets, err := mkcli.Market.Fetch()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	for _, i := range line.Snapshots {
-		fmt.Println(i)
+
+	for _, m := range markets {
+		fmt.Println(m)
 	}
 
-	for data := range mkcli.RealTimeQuotes.Fetch() {
-		fmt.Println(time.Now().Format("2006-01-02 15:04:05"), data)
-	}
+	//mkcli.RealTimeQuotes.Set([]string{"0.002957", "1.600360"})
+	//defer mkcli.RealTimeQuotes.Close()
+
+	//line, err := mkcli.Kline.Fetch("0.002957", "15", "120")
+	//if err != nil {
+	//	fmt.Println(err)
+	//	return
+	//}
+	//for _, i := range line.Snapshots {
+	//	fmt.Println(i)
+	//}
+	//
+	//for data := range mkcli.RealTimeQuotes.Fetch() {
+	//	fmt.Println(time.Now().Format("2006-01-02 15:04:05"), data)
+	//}
 
 }
